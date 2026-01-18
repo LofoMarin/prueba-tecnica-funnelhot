473de357a57d50c0edcc93237818c7d728b6b3ea
Luis Marin <84251099+LofoMarin@users.noreply.github.com>
2026-01-18 17:46:31 +0000

    ✨ Actualización de la configuración del proyecto y mejoras en el archivo .gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
- 
+  // Configuración de Next.js para Funnelhot
 }
 
 export default nextConfig
